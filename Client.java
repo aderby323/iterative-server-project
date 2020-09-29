84b912aa6140c875f213f2ddf0888f7296d03510
Alex Derby <aderby323@gmail.com>
2020-09-29 19:00:00 -0400

    Edited Client and Server files

--- Client.java
+++ Client.java
@@ -2,7 +2,18 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Hello world.");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter port: ");
+        int port = Integer.parseInt(reader.readLine());
+
+        Socket socket = new Socket("localhost", port);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+        String serverRespone = input.readLine();
+        System.out.println(serverRespone);
+
+        socket.close();
     }
 }
--- Server.java
+++ Server.java
@@ -1,8 +1,27 @@
 import java.net.*;
+import java.util.Date;
 import java.io.*;
 
 public class Server {
-    public void start() {
-        System.out.println("Test");
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter port: ");
+        int port = Integer.parseInt(reader.readLine());
+        
+        ServerSocket server = new ServerSocket(port);
+
+        System.out.println("[Server] Waiting for a connection.");
+        Socket socket = server.accept();
+        System.out.println("[Server] Connected to client.");
+
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+        String date = new Date().toString();
+        System.out.println("[Server] Sending date: " + date);
+        output.println(date);
+
+        System.out.println("[Server] Closing connection.");
+        socket.close();
+        server.close();
     }
 }
