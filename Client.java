9b97740baf0adeff5213342314f5c9558771645c
Alex Derby <aderby323@gmail.com>
2020-10-07 02:11:29 -0400

    Added client commands
    Added required client commands to server, some still placeholders.

--- Client.java
+++ Client.java
@@ -10,10 +10,17 @@ public class Client {
         Socket socket = new Socket("139.62.210.153", port);
 
         BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
         
-        String serverRespone = input.readLine();
-        System.out.println(serverRespone);
+        while (true) {
+            String command = reader.readLine();
 
+            output.println(command);
+            
+            String serverResponse = input.readLine();
+            if (serverResponse.equalsIgnoreCase("exit")) { break; }
+            System.out.println(serverResponse);
+        }
         socket.close();
     }
 }
--- Server.java
+++ Server.java
@@ -1,6 +1,9 @@
 import java.net.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.*;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 public class Server {
 
@@ -10,18 +13,53 @@ public class Server {
         int port = Integer.parseInt(reader.readLine());
         
         ServerSocket server = new ServerSocket(port);
+        RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
 
         System.out.println("[Server] Waiting for a connection.");
         Socket socket = server.accept();
         System.out.println("[Server] Connected to client.");
 
         PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-        String date = new Date().toString();
-        System.out.println("[Server] Sending date: " + date);
-        output.println(date);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        System.out.println("[Server] Closing connection.");
-        socket.close();
-        server.close();
+        try {
+            while (true) {
+                String request = input.readLine();
+                if (request.equalsIgnoreCase("exit")) { 
+                    output.println("exit");
+                    break; 
+                }
+                switch (request.toLowerCase()) {
+                    case "datetime":
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+                        output.println(dateFormat.format(new Date()));
+                        break;
+                    case "uptime":
+                        long uptime = runtimeMXBean.getUptime();
+                        output.println(uptime);
+                        break;
+                    case "memoryuse":
+                        output.println((double) Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+                        break;
+                    case "netstat":
+                        Process process = Runtime.getRuntime().exec("netstat");
+                        output.println();
+                        break;
+                    case "currentusers":
+                        output.println();
+                        break;
+                    case "runningprocesses":
+                        output.println();
+                        break;
+                    default:
+                        output.println("Unknown request.");
+                        break;
+                }
+            }
+        } finally {
+            System.out.println("[Server] Closing connection.");
+            socket.close();
+            server.close();
+        }
     }
 }
