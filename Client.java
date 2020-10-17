5f6abd22900ec611f1070430ad9622e8dd845d7b
Alex Derby <aderby323@gmail.com>
2020-10-16 21:47:50 -0400

    Added ClientConnection class
    Added the ClientConnection class for Client to spawn.

--- Client.java
+++ Client.java
@@ -1,4 +1,6 @@
 import java.net.*;
+import java.time.Duration;
+import java.time.Instant;
 import java.io.*;
 import java.util.Scanner;
 
@@ -8,70 +10,24 @@ import java.util.Scanner;
  * CNT 4504: Computer Networks and Distributed Processing 
  * Professor John Scott Kelly 
  * Alexander Derby, Afsara Chowdhurry, Emily Ottesen 
- * 10/13/2020
+ * 10/16/2020
  *
  * */
 
-public class Client extends Thread {
-
-    String address;
-    String response;
-    Socket socket;
-    BufferedReader input;
-    DataOutputStream output;
-    int port;
-
-    // Creates a new Client that connects to the Server, sends a response, and gets a response
-    public Client(String response, String address, int port) throws IOException {
-        this.address = address;
-        this.port = port;
-        this.response = response;
-        socket = new Socket(address, port);
-        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-    }
-
-    @Override
-    public void run() {
-        boolean isFinished = false;
-        String serverResponse;
-
-        // Send the user's response to Server
-        try {
-            output.writeUTF(response);
-            output.flush();
-
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-
-        // Wait for the server to send a response and print each line
-        while (!isFinished) {
-            try {
-                while ((serverResponse = input.readLine()) != null) {
-                    System.out.println(serverResponse);
-                }
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-            isFinished = true;
-        }
-
-        // Kill the connection
-        try { socket.close(); } 
-        catch (IOException ioException) { ioException.printStackTrace(); }
-    }
+public class Client {
 
     // Get IP address, port number, and user response
     public static void main(String[] args) throws IOException, InterruptedException {
         
         Scanner input = new Scanner(System.in);
-        Client[] clients = new Client[25];
+        ClientConnection[] clients = new ClientConnection[25];
 
         System.out.print("Enter address: ");
         String address = input.nextLine();
         System.out.print("Enter port number: ");
         int port = input.nextInt();
+
+        Socket socket = new Socket(address, port);
 
         while (true) {
 
@@ -86,34 +42,79 @@ public class Client extends Thread {
             System.out.println("================================================================================");
             
             System.out.print("\nEnter a command: ");
-            String userInput = input.nextLine();
+            String userInput = input.next();
             if (userInput.equalsIgnoreCase("quit")) { break; }
-            System.out.println("Number of clients to spawn: ");
+            System.out.println("\nNumber of clients to spawn: ");
             int clientsToCreate = input.nextInt();
 
-            // Create n Clients that user requests
             for (int i = 0; i < clientsToCreate; i++) {
-                Client spawnedClient = new Client(userInput, address, port);
-                clients[i] = spawnedClient;
+                ClientConnection client = new ClientConnection(socket, userInput);
+                clients[i] = client;
             }
 
-            // Start each thread
-            for (Client client : clients){
+            for (ClientConnection client : clients) {
                 client.start();
             }
 
-            // Wait for each thread to die
-            for (Client client : clients) {
+            for(ClientConnection client : clients) {
                 client.join();
-            }
-
-            // Remove existing clients from array
-            for (int i = 0; i < clientsToCreate; i++) {
-                clients[i] = null;
             }
         }
 
         //Close the Scanner
         input.close();
     }
+}
+
+class ClientConnection extends Thread {
+
+    private String response;
+    private Socket socket;
+    private BufferedReader input;
+    private DataOutputStream output;
+
+    public ClientConnection(Socket socket, String response) throws IOException {
+        this.socket = socket;
+        this.response = response;
+        input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+        String serverResponse;
+        boolean retrievedResonse = false;
+        Instant start = Instant.now();
+
+        // Send the user's response to Server
+        try {
+            output.writeUTF(response);
+            output.flush();
+
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        // Wait for the server to send a response, print each line,
+        // and record the Turn-around time.
+
+        try {
+            while (!retrievedResonse) {
+                while ((serverResponse = input.readLine()) != null) {
+                    System.out.println(serverResponse);
+                    retrievedResonse = true;
+                }
+            }   
+            Instant finish = Instant.now();
+            System.out.println(Duration.between(start, finish).toMillis());
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        // Kill the connection
+        try { socket.close(); } 
+        catch (IOException ioException) { ioException.printStackTrace(); }
+    }
+
+
 }
